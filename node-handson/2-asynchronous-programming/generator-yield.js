09cd8653b4198e1c4216fb4db6381ae93a5b33e7
atsukiwi <atsuki.hattori@daedalus.co.jp>
2023-04-23 17:17:29 +0900

    handle generator func

--- node-handson/2-asynchronous-programming/generator-yield.js
+++ node-handson/2-asynchronous-programming/generator-yield.js
@@ -21,13 +21,26 @@ function* asyncWithGeneratorFunc(json) {
     }
 }
 
-const asyncWithGenerator1 = asyncWithGeneratorFunc('{"foo":1}')
-const promise1 = asyncWithGenerator1.next().value
-// console.log(promise1)
-setTimeout(() => {
-    // then の戻り値は登録したコールバックで解決した新しいPromiseインスタンス
-    // そのため、この時点では pending
-    console.log(promise1.then(result => asyncWithGenerator1.next(result)))
-    // console.log(promise1)
-}, 2000)
+// const asyncWithGenerator1 = asyncWithGeneratorFunc('{"foo":1}')
+// const promise1 = asyncWithGenerator1.next().value
+// // console.log(promise1)
+// setTimeout(() => {
+//     // then の戻り値は登録したコールバックで解決した新しいPromiseインスタンス
+//     // そのため、この時点では pending
+//     console.log(promise1.then(result => asyncWithGenerator1.next(result)))
+//     // console.log(promise1)
+// }, 2000)
 
+function handleAsyncWithGenerator(generator, resolved) {
+    const { done, value } = generator.next(resolved)
+    if (done) {
+        return Promise.resolve(value)
+    }
+    return value.then(
+        resolved => handleAsyncWithGenerator(generator, resolved),
+        err => generator.throw(err)
+    )
+}
+
+console.log(handleAsyncWithGenerator(asyncWithGeneratorFunc('{"foo": 1}')))
+console.log(handleAsyncWithGenerator(asyncWithGeneratorFunc('不正なJSON')))
